7f4a4245b0160bd9e0d04e093b74d7d8e7191d6c
JwowSquared <Jdwagner1@hotmail.com>
2020-11-11 16:52:28 +0000

    added task 6 source code

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder - applies a function to all nodes in a binary tree
+* @tree: root of tree to apply function to
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
